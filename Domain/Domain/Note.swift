c0b0ecfa2185531150a5b2bc71fd7fd73af2bd13
YourLine <warrdadlani@gmail.com>
2024-06-11 21:11:32 +0100

    Added Modules
    Added presentation, data, and utility modules

---
+++ Domain/Domain/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Domain
+//
+//  Created by Warrd Adlani on 11/06/2024.
+//
+
+import Foundation
+
+public struct Note {
+    let title: String
+    let content: String
+}
---
+++ Utility/Utility/Utility.swift
@@ -0,0 +1,8 @@
+//
+//  Utility.swift
+//  Utility
+//
+//  Created by Warrd Adlani on 11/06/2024.
+//
+
+import Foundation
